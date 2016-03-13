7f4a38b590bc03e622a15ae5265f429742839fb3
Chiao Cheng <chiao@alum.mit.edu>
2016-03-13 11:12:14 -0400

    working fit without count-based overfit

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 
 from keras.models import Sequential
-from keras.layers.core import Dense, Activation
+from keras.layers.core import Dense, Activation, Dropout
 import numpy as np
-import random
 from sklearn.cross_validation import train_test_split
 
 ncols = 7
@@ -19,13 +18,12 @@ def parse_game(line0):
 
 
 def moves_to_state(moves):
-    state0 = [0] * state_dim
+    state0 = np.zeros(state_dim * 2)
     top_cells = [nrows * i for i in range(ncols)]
-    side = 1
-    moves = moves[:-random.randint(0, 1)]
+    side = 0
     for move0 in moves:
-        state0[top_cells[move0]] = side
-        side = {1: 2, 2: 1}[side]
+        state0[top_cells[move0] * 2 + side] = 1
+        side = (2 - side) // 2
         top_cells[move0] += 1
     return state0
 
@@ -37,7 +35,7 @@ def construct_model(nlayers, optimizer="sgd"):
             Dense(
                 output_dim=state_dim * 2,
                 init='lecun_uniform',
-                input_dim=state_dim * 2 if i == 0 else None
+                input_dim=2 * state_dim if i == 0 else None
             )
         )
         model.add(Activation('relu'))
@@ -48,20 +46,26 @@ def construct_model(nlayers, optimizer="sgd"):
     return model
 
 games = [parse_game(line0) for line0 in file("RvR.txt").readlines()]
-y_all, X_all = zip(*games)
-y_all = np.array(y_all)
-X_all = np.array([moves_to_state(moves0) for moves0 in X_all])
-X_all = np.concatenate([(X_all == i) for i in [1, 2]], axis=1) * 1
-y_all = np.array([(y_all == i) for i in range(3)]).T * 1
+y_raw, X_raw = zip(*games)
+y_raw = np.array(y_raw)
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X_all, y_all, train_size=10000, test_size=10000, random_state=42
+censor = np.random.randint(0, 2, len(X_raw))
+
+X_all = np.array([moves_to_state(moves0[:(-n if n > 0 else None)])
+                  for n, moves0 in zip(censor, X_raw)])
+# X_all = np.array([moves_to_state(moves0) for moves0 in X_raw])
+y_all = np.array([(y_raw == i) for i in range(3)]).T * 1
+
+X_train, X_test, y_train, y_test, censor_train, censor_test = train_test_split(
+    X_all, y_all, censor, test_size=1000,
+    random_state=42,
 )
 
 # sgd = SGD(lr=0.1, decay=0, momentum=0.1, nesterov=True)
-model0 = construct_model(2, optimizer="adam")
+model0 = construct_model(5, optimizer="adam")
 model0.fit(
-    X_train, y_train, nb_epoch=30,
+    X_train, y_train,
+    nb_epoch=5,
     validation_data=(X_test, y_test),
     show_accuracy=True,
 )
