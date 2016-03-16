23974cd3e91d8ae10fc6de052dd066a6ccfa6e76
Chiao Cheng <chiao@alum.mit.edu>
2016-03-16 09:24:50 -0400

    non-converging DQN

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 
 from keras.models import Sequential
-from keras.layers.core import Dense, Activation, Flatten
+from keras.layers.core import Dense, Activation, Flatten, Reshape
 from keras.layers import Convolution2D
-from keras.callbacks import EarlyStopping
 import numpy as np
-from sklearn.cross_validation import train_test_split
 
 ncols = 7
 nrows = 6
@@ -18,39 +16,18 @@ def parse_game(line0):
     return winner, moves
 
 
-def moves_to_state(moves):
+def moves_to_states(moves, n=1):
     state0 = np.zeros((2, nrows, ncols))
-    top_cells = [0 for _ in range(ncols)]
-    side = 0
-    for move0 in moves:
-        state0[side, top_cells[move0], move0] = 1
-        side = (2 - side) // 2
+    top_cells = [0] * ncols
+    if len(moves) - n == -1:
+        yield state0
+        state0 = state0.copy()
+    for i, move0 in enumerate(moves):
+        state0[i % 2, top_cells[move0], move0] = 1
         top_cells[move0] += 1
-    return state0
-
-
-def censor_data(ncensor, X_raw, y_raw):
-    censor = np.random.randint(0, ncensor + 1, len(X_raw))
-    X_all = np.array([
-        moves_to_state(moves0[:(-n if n > 0 else None)])
-        for n, moves0 in zip(censor, X_raw)
-    ])
-    # y_all = np.array([(y_raw == i) for i in range(3)]).T * 1
-    y_all = y_raw
-
-    (
-        X_train,
-        X_test,
-        y_train,
-        y_test,
-        censor_train,
-        censor_test
-    ) = train_test_split(
-        X_all, y_all, censor,
-        test_size=10000,
-        # random_state=42,
-    )
-    return X_train, X_test, y_train, y_test, censor_train, censor_test
+        if i >= len(moves) - n:
+            yield state0
+            state0 = state0.copy()
 
 
 def get_model():
@@ -65,36 +42,62 @@ def get_model():
     model.add(Flatten())
     model.add(Dense(8, init='lecun_uniform'))
     model.add(Activation('relu'))
-    model.add(Dense(1, init='lecun_uniform'))
-    model.add(Activation('sigmoid'))
-    model.compile(loss='binary_crossentropy',
-                  optimizer="adam",
-                  class_mode="binary")
+    model.add(Dense(ncols, init='lecun_uniform'))
+    model.add(Activation('linear'))
+    model.compile(loss='mse', optimizer="adam")
     return model
 
-model = get_model()
+
+def sars((winner0, moves0)):
+    nmoves = len(moves0)
+    sample_move = -(np.random.randint(0, nmoves - 1) + 1)
+    col0 = moves0[sample_move - 1]
+    if sample_move == -1:
+        state0 = moves_to_states(moves0[:sample_move]).next()
+        state1 = None
+        if winner0 == 1:
+            reward = 1
+        elif winner0 == 0:
+            reward = 0.5
+        else:
+            reward = 0
+        return state0, col0, reward, state1
+    else:
+        state0, state1 = list(moves_to_states(moves0[:sample_move], 2))
+        reward = 0
+        return state0, col0, reward, state1
+
+
+def gen_batch(games, n):
+    sample_games = [games[i] for i in np.random.randint(len(games), size=n)]
+    return map(sars, sample_games)
 
 games = [parse_game(line0) for line0 in file("RvR.txt").readlines()]
-y_raw, X_raw = zip(*games)
-y_raw = np.array(y_raw)
-# y_raw = np.select([y_raw == 1, y_raw == 0], [1., 0.5])
-y_raw = (y_raw == 1) * 1
 
-(
-    X_train,
-    X_test,
-    y_train,
-    y_test,
-    censor_train,
-    censor_test
-) = censor_data(4, X_raw, y_raw)
+alpha = 0.8
 
-model.fit(
-    X_train,
-    y_train,
-    batch_size=128,
-    nb_epoch=100,
-    validation_data=(X_test, y_test),
-    show_accuracy=True,
-    callbacks=[EarlyStopping(patience=10)],
-)
+model = get_model()
+for i in range(1000000):
+    state0s, actions, rewards, state1s = zip(*gen_batch(games, 10000))
+    state0s = np.array(state0s)
+
+    non_term_indices, non_term_states = zip(
+        *((i, state1)
+          for i, state1 in enumerate(state1s)
+          if state1 is not None)
+    )
+    non_term_indices = np.array(non_term_indices)
+    non_term_states = np.array(non_term_states)
+
+    Q1s = np.zeros(len(state1s))
+    Q1s[non_term_indices] = model.predict(non_term_states).max(axis=1)
+
+    Q0s = model.predict(np.array(state0s))
+
+    to_update = zip(*enumerate(actions))
+
+    Q0s[to_update] = np.array(rewards) + alpha * Q1s
+
+    print "Iter {0}, Error: {1[0]}".format(
+        i, model.train_on_batch(state0s, Q0s)
+    )
