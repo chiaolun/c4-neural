949ac4a24038db500f7f27277e0a9039b44f9e91
Chiao Cheng <chiao@alum.mit.edu>
2016-03-20 15:26:50 -0400

    afterstate sarsa

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ def moves_to_state(moves, state0=np.zeros((2, nrows, ncols), dtype="int8")):
     top_cells = state0.max(axis=0).argmin(axis=0)
     side = top_cells.sum() % 2
     for move0 in moves:
+        if top_cells[move0] == nrows:
+            return None
         state0[side, top_cells[move0], move0] = 1
         top_cells[move0] += 1
         side = 1 - side
@@ -37,26 +39,25 @@ def flip_state(state0):
     return state0[::flip0]
 
 
-def sars((winner0, moves0)):
+def srs((winner0, moves0)):
     no_state = np.zeros((2, nrows, ncols))
     sample_move = np.random.randint(0, len(moves0))
-    col0 = moves0[sample_move]
     state0 = moves_to_state(moves0[:sample_move])
     side0 = state0.sum() % 2
     flip0 = 1 - 2 * side0
     moves0 = moves0[sample_move:]
     if len(moves0) == 2:
-        return state0[::flip0], col0, 1, no_state
+        return state0[::flip0], 1, no_state
     elif len(moves0) == 1:
-        return state0[::flip0], col0, -1, no_state
+        return state0[::flip0], -1, no_state
     else:
         state1 = moves_to_state(moves0[:2], state0)
-        return state0[::flip0], col0, 0, state1[::flip0]
+        return state0[::flip0], 0, state1[::flip0]
 
 
 def gen_batch(games, n):
     sample_games = [games[i] for i in np.random.randint(len(games), size=n)]
-    return map(sars, sample_games)
+    return map(srs, sample_games)
 
 
 def iterate_minibatches(*arrays, **options):
@@ -89,7 +90,7 @@ def get_network():
         W=lasagne.init.GlorotUniform()
     )
     network = lasagne.layers.DenseLayer(
-        network, num_units=ncols,
+        network, num_units=1,
         nonlinearity=None,
     )
     return network
@@ -98,7 +99,25 @@ def get_network():
 def compile_Q(network):
     state = T.tensor4('state')
     Q = lasagne.layers.get_output(network, inputs=state)
-    Q_fn = theano.function([state], Q)
+    t_fn = theano.function([state], Q)
+
+    def Q_fn(state0s):
+        Qss = []
+        for state0 in state0s:
+            state1s = [
+                moves_to_state([i], state0)
+                for i in range(ncols)
+            ]
+            idx, non_zeros = zip(*[
+                (i, state1)
+                for i, state1 in enumerate(state1s)
+                if state1 is not None
+            ])
+            Qs = np.empty(ncols)
+            Qs.fill(np.nan)
+            Qs.flat[np.array(idx)] = t_fn(np.array(non_zeros))
+            Qss.append(Qs)
+        return np.array(Qss)
     return Q_fn
 
 
@@ -106,27 +125,16 @@ def compile_trainer(network):
     # Prepare Theano variables for inputs and targets
     alpha = T.scalar("alpha")
     state0 = T.tensor4('state0')
-    action = T.bvector('action')
     reward = T.vector('reward')
-    # state1 = T.tensor4('state1')
-    Q1max = T.vector("Q1max")
+    state1 = T.tensor4('state1')
 
     # Create a loss expression for training, i.e., a scalar objective
     # we want to minimize
-    Q0 = lasagne.layers.get_output(network, inputs=state0)
-    # Q1 = lasagne.layers.get_output(network, inputs=state1)
-    # Q1max = T.switch(
-    #     T.eq(state1.sum(axis=(1, 2, 3)), 0.9),
-    #     0., Q1.max(axis=1)
-    # )
+    Q0 = lasagne.layers.get_output(network, inputs=state0).flatten()
+    Q1 = lasagne.layers.get_output(network, inputs=state1).flatten()
 
-    # Q0[action] == reward + alpha * max(Q1) + error
     error_vec = (
-        (
-            Q0 *
-            T.extra_ops.to_one_hot(action, ncols)
-        ).sum(axis=1) -
-        reward - alpha * Q1max
+        Q0 - reward - alpha * Q1
     )
     error = (error_vec**2).mean()
 
@@ -139,7 +147,7 @@ def compile_trainer(network):
     # (by giving the updates dictionary) and returning the
     # corresponding training loss:
     train_fn = theano.function(
-        [state0, action, reward, Q1max, alpha],
+        [state0, reward, state1, alpha],
         error,
         updates=updates,
         on_unused_input='warn')
@@ -170,24 +178,19 @@ def load_network():
 class network_trainer():
 
     def __init__(self, network):
-        self.Q_fn = compile_Q(network)
         self.train_fn = compile_trainer(network)
 
     def train(
             self,
             state0s_batch,
-            actions_batch,
             rewards_batch,
             state1s_batch,
             alpha,
     ):
-        Q1max = self.Q_fn(state1s_batch).max(axis=1)
-        Q1max[state1s_batch.sum(axis=(1, 2, 3)) == 0] = 0.
         return self.train_fn(
             state0s_batch,
-            actions_batch,
             rewards_batch,
-            Q1max,
+            state1s_batch,
             alpha,
         )
 
@@ -205,9 +208,8 @@ def main(num_epochs=100):
     # We iterate over epochs:
     for epoch in range(num_epochs):
         start_time = time.time()
-        state0s, actions, rewards, state1s = zip(*gen_batch(games, 100000))
+        state0s, rewards, state1s = zip(*gen_batch(games, 100000))
         state0s = np.array(state0s, dtype=theano.config.floatX)
-        actions = np.array(actions, dtype="int8")
         rewards = np.array(rewards, dtype=theano.config.floatX)
         state1s = np.array(state1s, dtype=theano.config.floatX)
 
@@ -216,16 +218,14 @@ def main(num_epochs=100):
         train_batches = 0
         for (
                 state0s_batch,
-                actions_batch,
                 rewards_batch,
                 state1s_batch,
         ) in iterate_minibatches(
-                state0s, actions, rewards, state1s,
+                state0s, rewards, state1s,
                 batchsize=500, shuffle=True,
         ):
             train_err += trainer.train(
                 state0s_batch,
-                actions_batch,
                 rewards_batch,
                 state1s_batch,
                 0.9
